404bf1bd0cd9a4843bca57cd295be3e8e5e2ca44
Karun Choudhary <Karunchoudhary2000@gmail.com>
2024-08-28 13:41:40 +0530

    hosting starts

--- app.js
+++ app.js
@@ -86,7 +86,7 @@ app.post('/upload', upload.single('image'), (req, res) => {
 });
 
 // Start the server
-const port = 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
 });
