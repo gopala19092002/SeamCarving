fb22b04fbd874cb8193864dca30776f4fd2c7987
KARUN CHOUDHARY <72781081+karunsehwag@users.noreply.github.com>
2024-08-28 15:59:10 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,4 @@
 #!/bin/bash
 
-# Install necessary dependencies (if any)
-sudo apt-get update
-sudo apt-get install -y build-essential
-
-# Compile your C++ code
-g++ seam_carving.cpp -o seam_carving `pkg-config --cflags --libs opencv4`
-
+node app.js
 
