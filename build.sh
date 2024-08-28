7475b4670167cd919fc85970293e9c6b7b263ce5
KARUN CHOUDHARY <72781081+karunsehwag@users.noreply.github.com>
2024-08-28 16:06:20 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-node app.js
+# Compile the C++ application
+g++ seam_carving.cpp -o seam_carving `pkg-config --cflags --libs opencv4`
 
+# Install Node.js dependencies
+npm install
+
+
+# Start the Node.js application
+node app.js
